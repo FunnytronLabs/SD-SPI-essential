abb2b700d3acf4be11b13071ffb30db402e80041
Funnytron Labs <167612794+FunnytronLabs@users.noreply.github.com>
2024-06-28 18:40:01 +1000

    Create setup_SD_SPI.ino

---
+++ examples/setup_SD_SPI.ino
@@ -0,0 +1,27 @@
+#include "SD_SPI_essential.h"
+
+void setup(){
+  Serial.begin(115200);
+  while(!Serial) { delay (10); }
+
+  SD_SPI_initialise();
+
+  listDir(SD, "/", 0);
+  /*createDir(SD, "/mydir");
+  listDir(SD, "/", 0);
+  removeDir(SD, "/mydir");
+  listDir(SD, "/", 2);
+  writeFile(SD, "/hello.txt", "Hello ");
+  appendFile(SD, "/hello.txt", "World!\n");
+  readFile(SD, "/hello.txt");
+  deleteFile(SD, "/foo.txt");
+  renameFile(SD, "/hello.txt", "/foo.txt");
+  readFile(SD, "/foo.txt");
+  testFileIO(SD, "/test.txt");
+  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));*/
+}
+
+void loop(){
+  delay(100);
+}
